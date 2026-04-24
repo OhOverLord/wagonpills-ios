80fd36b8947431506e4201b42a0db18d15647a5e
Leonid Filippov <leonid.filippov@hptronic.cz>
2026-04-24 21:40:24 +0200

    Initial Commit

---
+++ Wagonpills/WagonpillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WagonpillsApp.swift
+//  Wagonpills
+//
+//  Created by Leonid Filippov on 24.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WagonpillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
